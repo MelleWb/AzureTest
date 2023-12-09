27aee4fc74def3683b852b86db54ebdbbea78795
Melle Wittebrood <mellewittebrood@hotmail.com>
2023-12-09 19:35:35 +0100

    change index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "lob-dev-app", "build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "lob-dev-app/build/", "index.html"));
 });
