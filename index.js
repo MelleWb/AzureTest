47ea0a16b782ad667afc33b4229811ff5401c4c5
Melle Wittebrood <mellewittebrood@hotmail.com>
2023-12-16 18:06:52 +0100

    werkt dit?

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 const cors = require("cors");
 app.use(cors());
@@ -12,6 +13,19 @@ app.use(
     extended: false,
   })
 );
+
+var db = mysql.createConnection({
+  host: "test-melle-database.mysql.database.azure.com",
+  user: "MelleCrud",
+  password: "Aezihw123!",
+  database: "employeesystem",
+  port: 3306,
+  ssl: {
+    ca: fs.readFileSync(
+      "C:\\Users\\melle\\OneDrive\\Documenten\\DigiCertGlobalRootCA.crt.pem"
+    ),
+  },
+});
 
 app.get("/hello", (req, res) => {
   res.send("Hello Server!");
@@ -28,8 +42,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-// const EmployeesRouter = require("./routes/Employees")(db);
-// app.use("/employees", EmployeesRouter);
+const EmployeesRouter = require("./routes/Employees")(db);
+app.use("/employees", EmployeesRouter);
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("/*", (req, res) => {
